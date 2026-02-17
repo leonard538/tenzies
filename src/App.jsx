8404d86614148f4567cc398d2618aaf5d8ef6f80
Leonard <nardpueblos@gmail.com>
2026-02-17 16:35:22 +0800

    added temp buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import Die from "./Die"
 
 function App() {
   return (
-    <main></main>
+    <main>
+      <Die value={1} />
+      <Die value={1} />
+      <Die value={1} />
+      <Die value={1} />
+      <Die value={1} />
+      <Die value={1} />
+      <Die value={1} />
+      <Die value={1} />
+      <Die value={1} />
+      <Die value={1} />
+    </main>
   )
 }
 

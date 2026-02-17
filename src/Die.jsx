73746e7511a6f6b75888d949b01adbc901ca4c88
Leonard <nardpueblos@gmail.com>
2026-02-17 17:19:06 +0800

    added nanoid and made props hold an object

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,20 +1,5 @@
-import Die from "./Die"
-
-export default function App() {
+export default function Die(props) {
     return (
-        <main>
-            <div className="dice-container">
-                <Die value={1} />
-                <Die value={2} />
-                <Die value={3} />
-                <Die value={4} />
-                <Die value={5} />
-                <Die value={6} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-            </div>
-        </main>
+        <button>{props.value}</button>
     )
 }

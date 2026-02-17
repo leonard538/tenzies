d29d9964024247378710179a5bb4c083009573bd
Leonard <nardpueblos@gmail.com>
2026-02-17 17:00:36 +0800

    fixed buttons and added roll dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 import Die from "./Die"
 
-function App() {
-  return (
-    <main>
-      <Die value={1} />
-      <Die value={1} />
-      <Die value={1} />
-      <Die value={1} />
-      <Die value={1} />
-      <Die value={1} />
-      <Die value={1} />
-      <Die value={1} />
-      <Die value={1} />
-      <Die value={1} />
-    </main>
-  )
+export default function App() {
+    
+    const [dice, setDice] = useState(generateAllNewDice())
+    
+    function generateAllNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => Math.ceil(Math.random() * 6))
+    }
+    
+    function rollDice() {
+        setDice(generateAllNewDice())
+    }
+    
+    const diceElements = dice.map(num => <Die value={num} />)
+    
+    return (
+        <main>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+            
+        </main>
+    )
 }
-
-export default App
